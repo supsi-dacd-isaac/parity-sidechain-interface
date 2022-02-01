66af9441f7a3e01acc4be5916cc8becc5eacfe65
dstreppa <Lasolita1976>
2022-02-01 16:10:16 +0100

    Fixed typo

--- scripts/pm/kpis_solver.py
+++ scripts/pm/kpis_solver.py
@@ -93,6 +93,8 @@ if __name__ == "__main__":
 
     # Eventually move tokens due to KPIs penalties
     logger.info('Total penalty: %s' % abs(penalty_amount[skm.local_account['name']]))
-    skm.send_tokens(skm.dso, abs(penalty_amount[skm.local_account['name']]))
+    amount = abs(penalty_amount[skm.local_account['name']])
+    if amount > 0:
+        skm.send_tokens(skm.dso, abs(penalty_amount[skm.local_account['name']]))
 
     logger.info('Ending program')
